38901d4e535c7b9cd064cad80b806e2151d1b36b
david <carew124@gmail.com>
2018-07-08 15:37:14 -0700

    Begin parser class

---
+++ src/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,14 @@
+package craftinginterpreters.lox;
+
+import java.util.List;
+
+import static craftinginterpreters.lox.TokenType.*;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens){
+        this.tokens = tokens;
+    }
+}
