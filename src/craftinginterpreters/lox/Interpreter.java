91e21adeb2bfbdb0ecb36271c455bb3d97260e42
david <carew124@gmail.com>
2018-07-10 17:08:38 -0700

    Add visitor method stubs

--- src/craftinginterpreters/lox/Interpreter.java
+++ src/craftinginterpreters/lox/Interpreter.java
@@ -1,4 +1,23 @@
 package craftinginterpreters.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        return null;
+    }
 }
