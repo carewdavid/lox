9bf25876b7948c60415242c258fafd9585c78f05
David Carew <carew124@gmail.com>
2018-07-28 22:21:08 -0700

    Add if statements to interpreter

--- src/craftinginterpreters/lox/Interpreter.java
+++ src/craftinginterpreters/lox/Interpreter.java
@@ -16,6 +16,17 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
 
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+
+        return null;
+    }
+
     private void executeBlock(List<Stmt> statements, Environment environment) {
         Environment prev = this.environment;
         try {
