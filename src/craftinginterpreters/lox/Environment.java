872c18e943ec3c6f18e734aa61936c2b9d00c403
David Carew <carew124@gmail.com>
2018-08-01 22:21:44 -0700

    Return in the right place after looking in outer scopes for assignments

--- src/craftinginterpreters/lox/Environment.java
+++ src/craftinginterpreters/lox/Environment.java
@@ -40,6 +40,7 @@ class Environment {
 
         if (enclosing != null) {
             enclosing.assign(name, value);
+            return;
         }
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
