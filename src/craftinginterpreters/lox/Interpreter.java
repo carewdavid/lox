1d87d89e68c05980e370313b757d782937523665
David Carew <carew124@gmail.com>
2018-08-01 21:29:13 -0700

    Fix stack overflow in assignment expressions

--- src/craftinginterpreters/lox/Interpreter.java
+++ src/craftinginterpreters/lox/Interpreter.java
@@ -68,7 +68,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
-        Object value = evaluate(expr);
+        Object value = evaluate(expr.value);
         environment.assign(expr.name, value);
         return value;
     }
