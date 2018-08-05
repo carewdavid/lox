96ffadff7368896ecec1f06e429b4ea7170d2380
David Carew <carew124@gmail.com>
2018-08-05 14:45:05 -0700

    Implement closures in interpreter

--- src/craftinginterpreters/lox/Interpreter.java
+++ src/craftinginterpreters/lox/Interpreter.java
@@ -83,8 +83,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitReturnStmt(Stmt.Return stmt) {
+        Object value = stmt.value == null ? null : evaluate(stmt.value);
+
+        /* Use an exception get back up to call() */
+        throw new Return(value);
+    }
+
+    @Override
     public Void visitFunctionStmt(Stmt.Function stmt) {
-        LoxFunction function = new LoxFunction(stmt);
+        LoxFunction function = new LoxFunction(stmt, environment);
         environment.define(stmt.name.lexeme, function);
         return null;
     }
--- src/craftinginterpreters/lox/LoxFunction.java
+++ src/craftinginterpreters/lox/LoxFunction.java
@@ -3,20 +3,26 @@ package craftinginterpreters.lox;
 import java.util.List;
 
 public class LoxFunction implements LoxCallable{
+    private final Environment closure;
     private final Stmt.Function declaration;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment closure) {
         this.declaration = declaration;
+        this.closure = closure;
     }
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment env = new Environment(interpreter.globals);
+        Environment env = new Environment(closure);
         for (int i = 0; i < declaration.parameters.size(); i++) {
             env.define(declaration.parameters.get(i).lexeme, arguments.get(i));
         }
 
-        interpreter.executeBlock(declaration.body, env);
+        try {
+            interpreter.executeBlock(declaration.body, env);
+        } catch (Return ret) {
+            return ret.value;
+        }
         return null;
     }
 
