50d5d1bef4447deb147b07d11ed71240913a2665
David Carew <carew124@gmail.com>
2018-08-15 13:42:12 -0700

    Add class for (runtime) class instances

--- src/craftinginterpreters/lox/LoxInstance.java
+++ src/craftinginterpreters/lox/LoxInstance.java
@@ -1,4 +1,14 @@
 package craftinginterpreters.lox;
 
 public class LoxInstance {
+    private LoxClass classs;
+
+    public LoxInstance(LoxClass classs) {
+        this.classs = classs;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s instance");
+    }
 }
