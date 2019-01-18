348242ed06094c8a181b5a17fe03936a6af2f675
David Carew <carew124@gmail.com>
2019-01-18 14:31:33 -0800

    Add stack to vm

--- clox/vm.c
+++ clox/vm.c
@@ -5,7 +5,22 @@
 
 VM vm;
 
+void push(Value value){
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop(){
+  vm.stackTop--;
+  return *vm.stackTop;
+}
+
+static void resetStack(){
+  vm.stackTop = vm.stack;
+}
+
 void initVM(){
+  resetStack();
 }
   
 void freeVM(){
@@ -15,14 +30,27 @@ static InterpretResult run(){
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
   for(;;){
+    #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for(Value *slot = vm.stack; slot < vm.stackTop; slot++){
+      printf("[");
+      printValue(*slot);
+      printf("]");
+    }
+    printf("\n");
+    
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+    #endif
     uint8_t instr;
     switch(instr = READ_BYTE()){
-    case OP_RETURN:
+    case OP_RETURN: {
+      printValue(pop());
+      printf("\n");
       return INTERPRET_OK;
+    }
     case OP_CONSTANT: {
       Value constant = READ_CONSTANT();
-      printValue(constant);
-      printf("\n");
+      push(constant);
       break;
     }
     }
