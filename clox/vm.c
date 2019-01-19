2250e00fb04703266ea36583e1eea3453f64448d
David Carew <carew124@gmail.com>
2019-01-18 16:59:26 -0800

    Add instructions for arithmetic operations to vm

--- clox/vm.c
+++ clox/vm.c
@@ -2,6 +2,7 @@
 
 #include "common.h"
 #include "vm.h"
+#include "debug.h"
 
 VM vm;
 
@@ -29,6 +30,13 @@ void freeVM(){
 static InterpretResult run(){
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+  double b = pop(); \
+  double a = pop(); \
+  push(a op b); \
+  }while(false)
+
   for(;;){
     #ifdef DEBUG_TRACE_EXECUTION
     printf("          ");
@@ -53,10 +61,35 @@ static InterpretResult run(){
       push(constant);
       break;
     }
+
+    case OP_NEGATE: {
+      push(-pop());
+      break;
+    }
+
+    case OP_ADD: {
+      BINARY_OP(+);
+      break;
+    }
+
+    case OP_SUBTRACT: {
+      BINARY_OP(-);
+      break;
+    }
+    case OP_MULTIPLY: {
+      BINARY_OP(*);
+      break;
+    }
+    case OP_DIVIDE: {
+      BINARY_OP(/);
+      break;
+    }
+
     }
   }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk){
