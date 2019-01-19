177abf3d57c0be6984860887f6b8c7324626f309
David Carew <carew124@gmail.com>
2019-01-19 13:03:39 -0800

    Lay down foundation for the lexer

--- clox/compiler.c
+++ clox/compiler.c
@@ -4,4 +4,18 @@
 
 void compile(const char *source){
   initScanner(scource);
+
+  //Basic driver for testing while the scanner is under construction
+  int line = -1;
+  for(;;){
+    Token token = scanToken();
+    if(token.line != line){
+      printf("%4d ", token.line);
+      line = token.line;
+    }else{
+      printf("   | ");
+    }
+    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+    if(token.type == TOKEN_EOF) break;
 }
---
+++ clox/scanner.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+#include "scanner.h"
+
+typedef struct {
+  const char *start;
+  const char *current;
+  int line;
+} Scanner;
+
+Scanner scanner;
+
+void initScanner(const char *source){
+  scanner.start = source;
+  scanner.current = source;
+  int line = 1;
+}
+
+static bool isAtEnd(){
+  return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type){
+  Token token;
+  token.type = type;
+  token.start = scanner.start;
+  token.length = (int)(scanner.current - scanner.start);
+  token.line scanner.line;
+  return token;
+}
+
+static errorToken(const char *msg){
+  Token token;
+  token.type = TOKEN_ERROR;
+  token.start = msg;
+  token.length = strlen(msg);
+  token.line = scanner.line;
+}
+
+Token scanToken(){
+  scanner.start = scanner.current;
+  if(isAtEnd()){
+    return makeToken(TOKEN_EOF);
+  }
+
+  return errorToken("Unexpected character");
+}
