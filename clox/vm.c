f2ec14509dfdb8a5104e733e7516d8d24c1748cd
David Carew <carew124@gmail.com>
2019-01-23 17:42:59 -0800

    Framework for bytecode compiler

--- clox/vm.c
+++ clox/vm.c
@@ -94,7 +94,18 @@ static InterpretResult run(){
 }
 
 InterpretResult interpret(char *source){
-  compile(source);
-  return INTERPRET_OK;
+  Chunk chunk;
+  initChunk(&chunk);
+  if(!compile(source, &chunk)){
+    freeChunk(&chunk);
+    return INTERPRET_COMPILE_ERROR;
+  }
+
+  vm.chunk = &chunk;
+  vm.ip = vm.chunk->code;
+  
+  InterpretResult result = run();
+  
+  return result;
 }
    
