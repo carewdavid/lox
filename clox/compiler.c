ff1159d1dd59a3981cf4c822ca11d20d7d2aaa9f
David Carew <carew124@gmail.com>
2019-01-24 17:27:37 -0800

    Parse unary expressions

--- clox/compiler.c
+++ clox/compiler.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 #include "common.h"
+#include "chunk.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "value.h"
 
 typedef struct {
   Token current;
@@ -14,13 +17,31 @@ typedef struct {
 
 Parser parser;
 
+//Operator precedence levels, lowest to highest
+typedef enum {
+	      PREC_NONE,
+	      PREC_ASSIGN,
+	      PREC_OR,
+	      PREC_AND,
+	      PREC_EQ,
+	      PREC_CMP,
+	      PREC_TERM,
+	      PREC_FACTOR,
+	      PREC_UNARY,
+	      PREC_CALL,
+	      PREC_PRIMARY
+} Precedence;
+
 Chunk *compilingChunk;
 
 static Chunk *currentChunk(){
   return compilingChunk;
 }
 
+//Emit error message with given line number
 static void errorAt(const Token *token, const char *message){
+  //If an error has previously occured, do nothing. We already know the parser is
+  //in a bad state, so there's no point
   if(parser.panicMode){
     return;
   }
@@ -48,6 +69,7 @@ static void error(const char *message){
 }
 
 
+//Consume the next non-error token of input
 static void advance(){
   parser.previous = parser.current;
 
@@ -61,6 +83,7 @@ static void advance(){
   }
 }
 
+//Consume the next token only if it is the expected kind, error otherwise
 static void consume(TokenType type, const char *message){
   if(parser.current.type == type){
     advance();
@@ -70,6 +93,9 @@ static void consume(TokenType type, const char *message){
   errorAtCurrent(message);
 }
 
+/* Bytecode generation */
+
+//Write to the current chunk of bytecode
 static void emitByte(uint8_t byte){
   writeChunk(currentChunk(), byte, parser.previous.line);
 }
@@ -87,7 +113,60 @@ static void endCompiler(){
   emitReturn();
 }
 
-void compile(const char *source, Chunk *chunk){
+static uint8_t makeConstant(Value value){
+  int constant = addConstant(currentChunk(), value);
+  if(constant > UINT8_MAX){
+    error("Too many constants in one chunk");
+    return 0;
+  }
+  return (uint8_t) constant;
+}
+  
+static void emitConstant(Value value){
+  emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+/* Parser */
+/* (And compiler, really. We do the parsing and emit bytcode all in one pass) */
+
+static void parsePrecedence(Precedence precedence){
+  //TODO
+}
+
+static void expression(){
+  parsePrecedence(PREC_ASSIGN);
+}
+
+//Parse a number literal
+static void number(){
+  double value = strtod(parser.previous.start, NULL);
+  emitConstant(value);
+}
+
+//Parse a parenthesized expression
+static void grouping(){
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression");
+}
+
+static void unary(){
+  TokenType opType = parser.previous.type;
+
+  //Parse operand
+  parsePrecedence(PREC_UNARY);
+
+  //Then emit the operator--we're working off of a stack here
+  switch(opType){
+  case TOKEN_MINUS:
+    emitByte(OP_NEGATE);
+    break;
+  default:
+    return;
+  }
+}
+
+
+bool compile(const char *source, Chunk *chunk){
   initScanner(source);
   parser.hadError = false;
   parser.panicMode = false;
--- clox/compiler.h
+++ clox/compiler.h
@@ -1,5 +1,5 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
-void compile(const char *source, Chunk *chunk);
+bool compile(const char *source, Chunk *chunk);
 #endif
